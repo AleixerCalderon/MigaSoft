e4873fdee0a0d3f79e1e53af557fb60c79c6a620
Ed Pino <maopino82@gmail.com>
2024-09-01 20:39:58 -0500

    icons update

--- cliente/src/componets/Dashboard.jsx
+++ cliente/src/componets/Dashboard.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart, faUsers, faBox, faChartBar, faClipboardList, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import './dashboard.css'; // Importa el CSS
+import './dashboard.css';
+import icon1 from "../assets/icon-inventario.svg";
+import icon2 from "../assets/icon-roles.svg";
+import icon3 from "../assets/icon-bodega.svg";
+import icon4 from "../assets/icon-analitca.svg";
 
 const Dashboard = () => {
   return (
@@ -14,7 +16,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <Link to="/inventario" className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faShoppingCart} size="2x" className="text-dark" />
+              <img src={icon1} alt="Inventario"/>
               <h5 className="card-title text-dark">Inventario</h5>
             </div>
           </Link>
@@ -22,7 +24,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <Link to="/usuarios" className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faUsers} size="2x" className="text-dark" />
+              <img src={icon2} alt="Usuarios"/>
               <h5 className="card-title text-dark">Usuarios</h5>
             </div>
           </Link>
@@ -30,7 +32,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <Link to="/bodega" className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faBox} size="2x" className="text-dark" />
+              <img src={icon3} alt="Bodega"/>
               <h5 className="card-title text-dark">Bodega</h5>
             </div>
           </Link>
@@ -38,7 +40,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <Link to="/reportes" className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faChartBar} size="2x" className="text-dark" />
+              <img src={icon4} alt="Reportes"/>
               <h5 className="card-title text-dark">Reportes</h5>
             </div>
           </Link>
@@ -46,7 +48,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <div className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faClipboardList} size="2x" className="text-dark" />
+              <img src={icon5} alt="Ordenes"/>
               <h5 className="card-title text-dark">Órdenes</h5>
             </div>
           </div>
@@ -54,7 +56,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <div className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faCog} size="2x" className="text-dark" />
+              <img src={icon6} alt="Configuraciones"/>
               <h5 className="card-title text-dark">Configuraciones</h5>
             </div>
           </div>
@@ -62,7 +64,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <div className="card text-decoration-none">
             <div className="card-body">
-              <FontAwesomeIcon icon={faSignOutAlt} size="2x" className="text-dark" />
+              <img src={icon2} alt="cerrrar secion"/>
               <h5 className="card-title text-dark">Cerrar Sesión</h5>
             </div>
           </div>
@@ -71,6 +73,7 @@ const Dashboard = () => {
         <div className="col-md-4 mb-4">
           <div className="card text-decoration-none">
             <div className="card-body">
+              <img src={ico7} alt="toal de productos"/>
               <h5 className="card-title text-dark">Total productos</h5>
               <p className="text-dark" style={{ fontSize: '24px' }}>640</p>
             </div>
