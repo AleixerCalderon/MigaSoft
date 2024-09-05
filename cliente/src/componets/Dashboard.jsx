5040ce9ee0c3cb3300f7d394d291c691b44c9909
Ed Pino <maopino82@gmail.com>
2024-09-05 12:06:37 -0500

    dashboard finnished

--- cliente/src/componets/Dashboard.jsx
+++ cliente/src/componets/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import './dashboard.css';
+import logo from "../assets/logo-migasoft.png";
 import icon1 from "../assets/icon-inventario.svg";
 import icon2 from "../assets/icon-roles.svg";
 import icon3 from "../assets/icon-bodega.svg";
@@ -9,39 +10,50 @@ import icon4 from "../assets/icon-analitca.svg";
 import icon5 from "../assets/icon-ordenes.svg";
 import icon6 from "../assets/icon-user.svg";
 import icon7 from "../assets/icon-totalProductos.svg";
-import icon8 from "../assets/icon-cerrarSesion.svg";
 
 const Dashboard = () => {
+  const cards = [
+    { to: "/inventario", icon: icon1, title: "Inventario" },
+    { to: "/usuarios", icon: icon2, title: "Usuarios" },
+    { to: "/bodega", icon: icon3, title: "Bodega" },
+    { to: "/reportes", icon: icon4, title: "Reportes" },
+    { to: "/ordenes", icon: icon5, title: "Órdenes" },
+    { to: "/configuraciones", icon: icon6, title: "Configuraciones" },
+    { to: "/total-productos", icon: icon7, title: "Total productos", extra: <p className="text-light" style={{ fontSize: '24px' }}>640</p> }
+  ];
+
   return (
-    <div className="container mt-5">
+    <div className="container mt-5 p-3">
       <h1 className="text-dark mt-5 margin-60">Bienvenido</h1>
       <div className="row text-center">
-        {[
-          { to: "/inventario", icon: icon1, title: "Inventario" },
-          { to: "/usuarios", icon: icon2, title: "Usuarios" },
-          { to: "/bodega", icon: icon3, title: "Bodega" },
-          { to: "/reportes", icon: icon4, title: "Reportes" },
-          { to: "/ordenes", icon: icon5, title: "Órdenes" },
-          { to: "/configuraciones", icon: icon6, title: "Configuraciones" },
-          { to: "/cerrar-sesion", icon: icon8, title: "Cerrar Sesión" },
-          { to: "/total-productos", icon: icon7, title: "Total productos", extra: <p className="text-ligh" style={{ fontSize: '24px' }}>640</p> }
-        ].map((card, index) => (
-          <div key={index} className="col-md-4 mb-4">
-            <Link to={card.to} className="card text-decoration-none">
-              <div className="card-body">
-                <img src={card.icon} alt={card.title} />
-                <h5 className="card-title text-light">{card.title}</h5>
-                {card.extra}
+        <div className="container">
+          <div className="row justify-content-around">
+            {cards.slice(0, 4).map((card, index) => (
+              <div key={index} className="col-md-2 mb-4">
+                <Link to={card.to} className="card text-decoration-none" style={{ height: '200px', width: '200px' }}>
+                  <div className="card-body d-flex flex-column align-items-center justify-content-center">
+                    <img src={card.icon} alt={card.title} />
+                    <h5 className="card-title text-light">{card.title}</h5>
+                    {card.extra}
+                  </div>
+                </Link>
               </div>
-            </Link>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="button-group mt-4">
-        <Link to="/inventario" className="btn btn-light text-dark m-2">Inventario</Link>
-        <Link to="/usuarios" className="btn btn-secondary text-light m-2">Usuarios</Link>
-        <Link to="/bodega" className="btn btn-dark text-light m-2">Bodega</Link>
-        <Link to="/reportes" className="btn btn-info text-dark m-2">Reportes</Link>
+          <div className="row justify-content-around">
+            {cards.slice(4, 7).map((card, index) => (
+              <div key={index} className="col-md-3 mb-1">
+                <Link to={card.to} className="card text-decoration-none" style={{ height: '200px', width: '200px' }}>
+                  <div className="card-body d-flex flex-column align-items-center justify-content-center">
+                    <img src={card.icon} alt={card.title} />
+                    <h5 className="card-title text-light">{card.title}</h5>
+                    {card.extra}
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
