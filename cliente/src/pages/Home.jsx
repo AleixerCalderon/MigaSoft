312d82b8d0fe03c793b640b87a262c1160d5e227
Gionandez <gio.nandez23@gmail.com>
2024-09-06 12:38:37 -0500

    Se agregan estilos al home con un array por rol

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,10 +1,33 @@
 import { useEffect, useState } from "react";
 import Header from "../componets/Header";
+import Dashboard from "../componets/Dashboard";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Footer from "../componets/Footer";
+import IconUser from "../assets/icon-usuario-v2.svg";
+import IconInventario from "../assets/icon-inventario-v2.svg";
+import IconBodega from "../assets/icon-bodega-v2.svg";
+import IconAnalitica from "../assets/icon-analitca-v2.svg";
+import IconlistUsers from "../assets/icon-listUsers.svg";
 
 const HomePage = ()=>{
     const [usuario, setUsuario]=useState(null);
     const userId = 2;
+    const roles = ["admin", "bodeguero","vendedor","secretaria"];
+    const getRole =(rol)=>{
+        switch (rol) {
+            case "admin":
+                return(IconUser);
+            case "bodeguero":
+                return(IconInventario);
+            case "vendedor":
+                return(IconlistUsers);
+                
+            default:
+                return(null);
+        }
+    }
     useEffect(()=>{
         const fetchData = async ()=>{
             const userToken = localStorage.getItem('token');
@@ -30,8 +53,40 @@ const HomePage = ()=>{
     return(
         <>
             <Header/>
-            <h1>Este es el HomePage</h1>
-            {usuario ? <pre>{JSON.stringify(usuario, null, 1)}</pre>: <p>Cargando...</p>}
+            <Container fluid className="dashboard">
+                <Row>
+                    <Col xs={12}>
+                        <h1>Bienvenido</h1>
+                    </Col>
+                </Row>
+                <Row>
+                        {roles.map(
+                            (rol)=>(
+                            <Col className="dash-menu">
+                                <div className="dash-menu-item">
+                                    <a href="#">
+                                        <img src={getRole(rol)} alt={rol} />
+                                        <h4>{rol}</h4>
+                                    </a>
+                                </div>  
+                            </Col>
+                        )
+                        )}
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="reporte-item">
+                            <img src={IconlistUsers} alt="" />
+                            <p>
+                                Usuarios creados
+                                <span>130</span>
+                            </p>
+                        </div>
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
             <Footer/>
         </>
     );
